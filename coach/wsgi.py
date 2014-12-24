bb1bb41bb790958b5da19dbff58599e582e666c8
Greg Levin <ljz00us@gmail.com>
2014-12-24 13:17:37 -0800

    shift to dr site

--- coach/wsgi.py
+++ coach/wsgi.py
@@ -13,11 +13,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coach.settings")
 from django.core.wsgi import get_wsgi_application
 
 # below was commented OUT to put onto Heroku ----------------------------------- ....
-
 # application = get_wsgi_application()
-
 # ... and then this was added FOR HEROKU -----------------------------------....
-#
+
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
